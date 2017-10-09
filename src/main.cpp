bfd680c430cb9c892c422a2181a38fd4382ab6c8
Kike Alcor <ealcor@gmail.com>
2017-10-09 21:37:03 +0200

    Basic SDL2 loop, show a green window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,50 @@
 #include <SDL.h>
+#include <stdio.h>
 
-int main(int argc, char **argv)
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
 {
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		window = SDL_CreateWindow("NESemu", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			screenSurface = SDL_GetWindowSurface(window);
+
+			bool quit = false;
+			SDL_Event e;
+
+			while (!quit)
+			{
+				while (SDL_PollEvent(&e) != 0)
+				{
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+				}
+
+				SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x77, 0x00));
+				SDL_UpdateWindowSurface(window);
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
 	return 0;
 }
