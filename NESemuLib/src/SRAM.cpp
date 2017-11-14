cbedbd27fe428686e2cd42cb072110f540ae7975
Kike Alcor <ealcor@gmail.com>
2017-11-14 22:41:24 +0100

    Parsing is now handled by NESemu directly

--- NESemuLib/src/SRAM.cpp
+++ NESemuLib/src/SRAM.cpp
@@ -15,14 +15,24 @@ SRAM::~SRAM()
 
 uint8_t SRAM::ReadMem(uint16_t address)
 {
-	uint16_t index = ConvertToIndex(address);
-	return _sram[index];
+	if (_enabled)
+	{
+		uint16_t index = ConvertToIndex(address);
+		return _sram[index];
+	}
+	else
+	{
+		return 0;
+	}
 }
 
 void SRAM::WriteMem(uint16_t address, uint8_t value)
 {
-	uint16_t index = ConvertToIndex(address);
-	_sram[index] = value;
+	if (_enabled)
+	{
+		uint16_t index = ConvertToIndex(address);
+		_sram[index] = value;
+	}
 }
 
 void SRAM::PowerOn()
@@ -31,6 +41,7 @@ void SRAM::PowerOn()
 	{
 		_sram[i] = 0;
 	}
+	_enabled = false;
 }
 
 void SRAM::Reset()
@@ -49,6 +60,7 @@ void SRAM::Load(const char* path)
 	fopen_s(&file, path, "rb");
 	fread_s(_sram, sizeof(_sram), sizeof(uint8_t), sizeofarray(_sram), file);
 	fclose(file);
+	_enabled = true;
 }
 
 void SRAM::Save(const char* path)
--- NESemuLibTests/SRAMTest.cpp
+++ NESemuLibTests/SRAMTest.cpp
@@ -14,6 +14,15 @@ namespace NESemuLibTests
 		{
 			SRAM sram;
 
+			// If disabled, all reads should return 0
+			sram.SetEnabled(false);
+			for (int i = 0; i < SRAM::kSize; ++i)
+			{
+				Assert::AreEqual((int)sram.ReadMem(SRAM::kStartAddress + i), 0);
+			}
+
+			// Enable, write some example data and save
+			sram.SetEnabled(true);
 			for (int i = 0; i < SRAM::kSize; ++i)
 			{
 				uint8_t value = i % 256;
@@ -21,6 +30,7 @@ namespace NESemuLibTests
 			}
 			sram.Save("test.bin");
 
+			// Load saved file and check contents
 			sram.PowerOn();
 			for (int i = 0; i < SRAM::kSize; ++i)
 			{
@@ -31,6 +41,13 @@ namespace NESemuLibTests
 			{
 				uint8_t value = i % 256;
 				Assert::AreEqual(sram.ReadMem(SRAM::kStartAddress + i), value);
+			}
+
+			// Disable and check it always returns 0 again
+			sram.SetEnabled(false);
+			for (int i = 0; i < SRAM::kSize; ++i)
+			{
+				Assert::AreEqual((int)sram.ReadMem(SRAM::kStartAddress + i), 0);
 			}
 		}
 
