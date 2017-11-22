c2e397fcc058b2be0d0e875551c16b880a45e6dc
Kike Alcor <ealcor@gmail.com>
2017-11-22 23:42:44 +0100

    Small refactor

--- NESemuLib/src/opcodes/LDY.cpp
+++ NESemuLib/src/opcodes/LDY.cpp
@@ -6,7 +6,7 @@ int CPU::LDY(AddressingMode mode)
     const uint8_t value = GetValueWithMode(mode, cycles);
     _y = value;
 
-    SetFlag(Flag::Sign, (_y & (1 << 7)) != 0);
+    SetFlag(Flag::Sign, IsValueNegative(_y) != 0);
     SetFlag(Flag::Zero, _y == 0);
 
     return cycles;
