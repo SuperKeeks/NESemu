e3a20cfddd82f83a7ef1dda443213684ce07d279
Kike Alcor <ealcor@gmail.com>
2017-12-09 00:48:28 +0100

    Small refactor

--- NESemuLib/src/RAM.cpp
+++ NESemuLib/src/RAM.cpp
@@ -12,20 +12,20 @@ RAM::~RAM()
 
 uint8_t RAM::ReadMem(uint16_t address)
 {
-    uint16_t realAddress = ConvertToRealAddress(address);
+    const uint16_t realAddress = ConvertToRealAddress(address);
     return _ram[realAddress];
 }
 
 void RAM::WriteMem(uint16_t address, uint8_t value)
 {
-    uint16_t realAddress = ConvertToRealAddress(address);
+    const uint16_t realAddress = ConvertToRealAddress(address);
     _ram[realAddress] = value;
 }
 
 uint16_t RAM::ConvertToRealAddress(uint16_t address) const
 {
     OMBAssert(address >= 0 && address < 0x2000, "Address is not within the RAM's address space");
-    return address % 2048; // Meant for Shadow RAM
+    return address % kRAMSize; // Meant for Shadow RAM
 }
 
 void RAM::PowerOn()
