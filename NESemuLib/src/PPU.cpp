ef742a827c00b6c03368d4e872e0c4bb2aeb2b50
Kike Alcor <ealcor@gmail.com>
2017-12-09 01:12:07 +0100

    PPU: Added registers and VRAM

--- NESemuLib/src/PPU.cpp
+++ NESemuLib/src/PPU.cpp
@@ -12,13 +12,76 @@ PPU::~PPU()
 
 uint8_t PPU::ReadMem(uint16_t address)
 {
-    OMBAssert(false, "Unimplemented");
-    return 0;
+    if (address == 0x2002)
+    {
+        return _ppuStatus;
+    }
+    else if (address == 0x2004)
+    {
+        _oamAddr++;
+        OMBAssert(false, "TODO");
+        return 0;
+    }
+    else if (address == 0x2007)
+    {
+        const uint16_t address = ConvertToRealVRAMAddress(_ppuAddr);
+        _ppuAddr += GetAddressIncrement();
+        return _vram[address];
+    }
+    else if (address >= 0x2008 && address < 0x4000)
+    {
+        // Shadow PPU Registers
+        return ReadMem(((address - 0x2000) % (0x2008 - 0x2000)) + 0x2000);
+    }
+    else
+    {
+        OMBAssert(false, "Unimplemented");
+        return 0;
+    }
 }
 
 void PPU::WriteMem(uint16_t address, uint8_t value)
 {
-    OMBAssert(false, "Unimplemented");
+    if (address == 0x2000)
+    {
+        _ppuCtrl = value;
+    }
+    else if (address == 0x2001)
+    {
+        _ppuMask = value;
+    }
+    else if (address == 0x2003)
+    {
+        _oamAddr = value;
+    }
+    else if (address == 0x2004)
+    {
+        _oamAddr++;
+        OMBAssert(false, "TODO");
+    }
+    else if (address == 0x2005)
+    {
+        OMBAssert(false, "TODO");
+    }
+    else if (address == 0x2006)
+    {
+        OMBAssert(false, "TODO");
+    }
+    else if (address == 0x2007)
+    {
+        const uint16_t address = ConvertToRealVRAMAddress(_ppuAddr);
+        _ppuAddr += GetAddressIncrement();
+        _vram[address] = value;
+    }
+    else if (address >= 0x2008 && address < 0x4000)
+    {
+        // Shadow PPU Registers
+        WriteMem(((address - 0x2000) % (0x2008 - 0x2000)) + 0x2000, value);
+    }
+    else
+    {
+        OMBAssert(false, "Unimplemented");
+    }
 }
 
 void PPU::PowerOn()
@@ -29,4 +92,28 @@ void PPU::PowerOn()
 void PPU::Reset()
 {
     // TODO
+}
+
+uint16_t PPU::ConvertToRealVRAMAddress(uint16_t address) const
+{
+    if (address >= 0x3F10 && address < 0x4000)
+    {
+        return ((address - 0x3F00) % (0x3F20 - 0x3F00)) + 0x3F00; // Shadow palette
+    }
+    else
+    {
+        return address % kVRAMSize; // Shadow VRAM
+    }
+}
+
+uint8_t PPU::GetAddressIncrement() const
+{
+    if ((_ppuCtrl & (1 << 2)) == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 32;
+    }
 }
--- NESemuLib/src/PPU.h
+++ NESemuLib/src/PPU.h
@@ -5,6 +5,8 @@
 class PPU : public MemoryHandler
 {
 public:
+    static const int kVRAMSize = 16384;
+
     PPU();
     virtual ~PPU();
 
@@ -12,4 +14,18 @@ public:
     virtual void WriteMem(uint16_t address, uint8_t value);
     virtual void PowerOn();
     virtual void Reset();
+
+private:
+    // Registers
+    uint8_t _ppuCtrl;
+    uint8_t _ppuMask;
+    uint8_t _ppuStatus;
+    uint8_t _oamAddr;
+    uint16_t _ppuScroll;
+    uint16_t _ppuAddr;
+
+    uint8_t _vram[kVRAMSize];
+
+    uint16_t ConvertToRealVRAMAddress(uint16_t address) const;
+    uint8_t GetAddressIncrement() const;
 };
