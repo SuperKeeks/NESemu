9e4f3cfc07d37aaec9dca6a2e1f039568b8fe462
Kike Alcor <ealcor@gmail.com>
2018-01-02 22:25:16 +0100

    Parameter rename

--- NESemuLib/src/BitwiseUtils.cpp
+++ NESemuLib/src/BitwiseUtils.cpp
@@ -1,18 +1,18 @@
 #include "BitwiseUtils.h"
 
-void BitwiseUtils::SetFlag(uint8_t& registerRef, int shift, bool value)
+void BitwiseUtils::SetFlag(uint8_t& registerRef, int bitIndex, bool value)
 {
     if (value)
     {
-        registerRef |= (1 << shift);
+        registerRef |= (1 << bitIndex);
     }
     else
     {
-        registerRef &= ~(1 << shift);
+        registerRef &= ~(1 << bitIndex);
     }
 }
 
-bool BitwiseUtils::IsFlagSet(uint8_t registerValue, int shift)
+bool BitwiseUtils::IsFlagSet(uint8_t registerValue, int bitIndex)
 {
-    return (registerValue & (1 << shift)) != 0;
+    return (registerValue & (1 << bitIndex)) != 0;
 }
--- NESemuLib/src/BitwiseUtils.h
+++ NESemuLib/src/BitwiseUtils.h
@@ -4,6 +4,6 @@
 
 namespace BitwiseUtils
 {
-    void SetFlag(uint8_t& registerRef, int shift, bool value);
-    bool IsFlagSet(uint8_t registerValue, int shift);
+    void SetFlag(uint8_t& registerRef, int bitIndex, bool value);
+    bool IsFlagSet(uint8_t registerValue, int bitIndex);
 }
