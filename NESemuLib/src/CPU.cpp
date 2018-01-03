3b6028e11549a9357f6efbe4b864de99b655c17c
Kike Alcor <ealcor@gmail.com>
2018-01-03 15:46:43 +0100

    Changed opcode execution to a big switch() instead of using an opcode to function map, as it was too slow when debugging

--- NESemuLib/src/CPU.cpp
+++ NESemuLib/src/CPU.cpp
@@ -38,22 +38,198 @@ void CPU::Tick()
 int CPU::ExecuteNextInstruction()
 {
     // Fetch next opcode
-    uint8_t opcode = _memoryHandler->ReadMem(++_programCounter);
-    int cycles = 0;
+    const uint8_t opcode = _memoryHandler->ReadMem(++_programCounter);
+    //Log::Debug("Executing opcode: $%02X", opcode);
 
-    auto opcodeFunction = _opcodes.find(opcode);
-    if (opcodeFunction != _opcodes.end())
+    switch (opcode)
     {
-        //Log::Debug("Executing opcode: $%02X", opcode);
-        cycles = opcodeFunction->second();
-        //Log::Debug("Took %d cycles", cycles);
-    }
-    else
-    {
-        Log::Debug("Unknown opcode: $%02X", opcode);
-    }
+        // LDA
+        case 0xA9: return LDA(AddressingMode::Immediate);
+        case 0xA5: return LDA(AddressingMode::ZeroPage);
+        case 0xB5: return LDA(AddressingMode::ZeroPageX);
+        case 0xAD: return LDA(AddressingMode::Absolute);
+        case 0xBD: return LDA(AddressingMode::AbsoluteX);
+        case 0xB9: return LDA(AddressingMode::AbsoluteY);
+        case 0xA1: return LDA(AddressingMode::IndirectX);
+        case 0xB1: return LDA(AddressingMode::IndirectY);
 
-    return cycles;
+        // LDX
+        case 0xA2: return LDX(AddressingMode::Immediate);
+        case 0xA6: return LDX(AddressingMode::ZeroPage);
+        case 0xB6: return LDX(AddressingMode::ZeroPageY);
+        case 0xAE: return LDX(AddressingMode::Absolute);
+        case 0xBE: return LDX(AddressingMode::AbsoluteY);
+
+        // LDY
+        case 0xA0: return LDY(AddressingMode::Immediate);
+        case 0xA4: return LDY(AddressingMode::ZeroPage);
+        case 0xB4: return LDY(AddressingMode::ZeroPageX);
+        case 0xAC: return LDY(AddressingMode::Absolute);
+        case 0xBC: return LDY(AddressingMode::AbsoluteX);
+
+        // STA
+        case 0x85: return STA(AddressingMode::ZeroPage);
+        case 0x95: return STA(AddressingMode::ZeroPageX);
+        case 0x8D: return STA(AddressingMode::Absolute);
+        case 0x9D: return STA(AddressingMode::AbsoluteX);
+        case 0x99: return STA(AddressingMode::AbsoluteY);
+        case 0x81: return STA(AddressingMode::IndirectX);
+        case 0x91: return STA(AddressingMode::IndirectY);
+
+        // STX
+        case 0x86: return STX(AddressingMode::ZeroPage);
+        case 0x96: return STX(AddressingMode::ZeroPageY);
+        case 0x8E: return STX(AddressingMode::Absolute);
+
+        // STY
+        case 0x84: return STY(AddressingMode::ZeroPage);
+        case 0x94: return STY(AddressingMode::ZeroPageX);
+        case 0x8C: return STY(AddressingMode::Absolute);
+
+        // ADC
+        case 0x69: return ADC(AddressingMode::Immediate);
+        case 0x65: return ADC(AddressingMode::ZeroPage);
+        case 0x75: return ADC(AddressingMode::ZeroPageX);
+        case 0x6D: return ADC(AddressingMode::Absolute);
+        case 0x7D: return ADC(AddressingMode::AbsoluteX);
+        case 0x79: return ADC(AddressingMode::AbsoluteY);
+        case 0x61: return ADC(AddressingMode::IndirectX);
+        case 0x71: return ADC(AddressingMode::IndirectY);
+
+        // SBC
+        case 0xE9: return SBC(AddressingMode::Immediate);
+        case 0xE5: return SBC(AddressingMode::ZeroPage);
+        case 0xF5: return SBC(AddressingMode::ZeroPageX);
+        case 0xED: return SBC(AddressingMode::Absolute);
+        case 0xFD: return SBC(AddressingMode::AbsoluteX);
+        case 0xF9: return SBC(AddressingMode::AbsoluteY);
+        case 0xE1: return SBC(AddressingMode::IndirectX);
+        case 0xF1: return SBC(AddressingMode::IndirectY);
+
+        // CPU Flags
+        case 0x18: return CLC(AddressingMode::Implied);
+        case 0x38: return SEC(AddressingMode::Implied);
+        case 0x58: return CLI(AddressingMode::Implied);
+        case 0x78: return SEI(AddressingMode::Implied);
+        case 0xB8: return CLV(AddressingMode::Implied);
+        case 0xD8: return CLD(AddressingMode::Implied);
+        case 0xF8: return SED(AddressingMode::Implied);
+
+        // Increment/Decrement
+        case 0xCA: return DEX(AddressingMode::Implied);
+        case 0xE8: return INX(AddressingMode::Implied);
+        case 0x88: return DEY(AddressingMode::Implied);
+        case 0xC8: return INY(AddressingMode::Implied);
+        case 0xC6: return DECINC(AddressingMode::ZeroPage, -1);
+        case 0xD6: return DECINC(AddressingMode::ZeroPageX, -1);
+        case 0xCE: return DECINC(AddressingMode::Absolute, -1);
+        case 0xDE: return DECINC(AddressingMode::AbsoluteX, -1);
+        case 0xE6: return DECINC(AddressingMode::ZeroPage, +1);
+        case 0xF6: return DECINC(AddressingMode::ZeroPageX, +1);
+        case 0xEE: return DECINC(AddressingMode::Absolute, +1);
+        case 0xFE: return DECINC(AddressingMode::AbsoluteX, +1);
+
+        // Routine instructions
+        case 0x4C: return JMP(AddressingMode::Absolute);
+        case 0x6C: return JMP(AddressingMode::Indirect);
+        case 0x20: return JSR(AddressingMode::Absolute);
+        case 0x60: return RTS(AddressingMode::Implied);
+        case 0x40: return RTI(AddressingMode::Implied);
+
+        // Compare instructions
+        case 0xC9: return CMP(AddressingMode::Immediate, _accumulator); // CMP
+        case 0xC5: return CMP(AddressingMode::ZeroPage, _accumulator);  // CMP
+        case 0xD5: return CMP(AddressingMode::ZeroPageX, _accumulator); // CMP
+        case 0xCD: return CMP(AddressingMode::Absolute, _accumulator);  // CMP
+        case 0xDD: return CMP(AddressingMode::AbsoluteX, _accumulator); // CMP
+        case 0xD9: return CMP(AddressingMode::AbsoluteY, _accumulator); // CMP
+        case 0xC1: return CMP(AddressingMode::IndirectX, _accumulator); // CMP
+        case 0xD1: return CMP(AddressingMode::IndirectY, _accumulator); // CMP
+        case 0xE0: return CMP(AddressingMode::Immediate, _x);           // CPX
+        case 0xE4: return CMP(AddressingMode::ZeroPage, _x);            // CPX
+        case 0xEC: return CMP(AddressingMode::Absolute, _x);            // CPX
+        case 0xC0: return CMP(AddressingMode::Immediate, _y);           // CPY
+        case 0xC4: return CMP(AddressingMode::ZeroPage, _y);            // CPY
+        case 0xCC: return CMP(AddressingMode::Absolute, _y);            // CPY
+
+        // Logic instructions
+        case 0x24: return BIT(AddressingMode::ZeroPage);
+        case 0x2C: return BIT(AddressingMode::Absolute);
+        case 0x29: return AND(AddressingMode::Immediate);
+        case 0x25: return AND(AddressingMode::ZeroPage);
+        case 0x35: return AND(AddressingMode::ZeroPageX);
+        case 0x2D: return AND(AddressingMode::Absolute);
+        case 0x3D: return AND(AddressingMode::AbsoluteX);
+        case 0x39: return AND(AddressingMode::AbsoluteY);
+        case 0x21: return AND(AddressingMode::IndirectX);
+        case 0x31: return AND(AddressingMode::IndirectX);
+        case 0x09: return ORA(AddressingMode::Immediate);
+        case 0x05: return ORA(AddressingMode::ZeroPage);
+        case 0x15: return ORA(AddressingMode::ZeroPageX);
+        case 0x0D: return ORA(AddressingMode::Absolute);
+        case 0x1D: return ORA(AddressingMode::AbsoluteX);
+        case 0x19: return ORA(AddressingMode::AbsoluteY);
+        case 0x01: return ORA(AddressingMode::IndirectX);
+        case 0x11: return ORA(AddressingMode::IndirectX);
+        case 0x49: return EOR(AddressingMode::Immediate);
+        case 0x45: return EOR(AddressingMode::ZeroPage);
+        case 0x55: return EOR(AddressingMode::ZeroPageX);
+        case 0x4D: return EOR(AddressingMode::Absolute);
+        case 0x5D: return EOR(AddressingMode::AbsoluteX);
+        case 0x59: return EOR(AddressingMode::AbsoluteY);
+        case 0x41: return EOR(AddressingMode::IndirectX);
+        case 0x51: return EOR(AddressingMode::IndirectX);
+
+        // Shift instructions
+        case 0x0A: return ASL(AddressingMode::Accumulator);
+        case 0x06: return ASL(AddressingMode::ZeroPage);
+        case 0x16: return ASL(AddressingMode::ZeroPageX);
+        case 0x0E: return ASL(AddressingMode::Absolute);
+        case 0x1E: return ASL(AddressingMode::AbsoluteX);
+        case 0x4A: return LSR(AddressingMode::Accumulator);
+        case 0x46: return LSR(AddressingMode::ZeroPage);
+        case 0x56: return LSR(AddressingMode::ZeroPageX);
+        case 0x4E: return LSR(AddressingMode::Absolute);
+        case 0x5E: return LSR(AddressingMode::AbsoluteX);
+        case 0x2A: return ROL(AddressingMode::Accumulator);
+        case 0x26: return ROL(AddressingMode::ZeroPage);
+        case 0x36: return ROL(AddressingMode::ZeroPageX);
+        case 0x2E: return ROL(AddressingMode::Absolute);
+        case 0x3E: return ROL(AddressingMode::AbsoluteX);
+        case 0x6A: return ROR(AddressingMode::Accumulator);
+        case 0x66: return ROR(AddressingMode::ZeroPage);
+        case 0x76: return ROR(AddressingMode::ZeroPageX);
+        case 0x6E: return ROR(AddressingMode::Absolute);
+        case 0x7E: return ROR(AddressingMode::AbsoluteX);
+
+        // Branch-on-condition instructions
+        case 0xF0: return BXX(AddressingMode::Relative, GetFlag(Flag::Zero));      // BEQ
+        case 0xD0: return BXX(AddressingMode::Relative, !GetFlag(Flag::Zero));     // BNE
+        case 0xB0: return BXX(AddressingMode::Relative, GetFlag(Flag::Carry));     // BCS
+        case 0x90: return BXX(AddressingMode::Relative, !GetFlag(Flag::Carry));    // BCC
+        case 0x70: return BXX(AddressingMode::Relative, GetFlag(Flag::Overflow));  // BVS
+        case 0x50: return BXX(AddressingMode::Relative, !GetFlag(Flag::Overflow)); // BVC
+        case 0x30: return BXX(AddressingMode::Relative, GetFlag(Flag::Sign));      // BMI
+        case 0x10: return BXX(AddressingMode::Relative, !GetFlag(Flag::Sign));     // BPL
+
+        // Transfer instructions
+        case 0x9A: return TXX(AddressingMode::Implied, _x, _stackPointer); // TXS
+        case 0xBA: return TXX(AddressingMode::Implied, _stackPointer, _x); // TSX
+        case 0xAA: return TXX(AddressingMode::Implied, _accumulator, _x);  // TAX
+        case 0x8A: return TXX(AddressingMode::Implied, _x, _accumulator);  // TXA
+        case 0xA8: return TXX(AddressingMode::Implied, _accumulator, _y);  // TAY
+        case 0x98: return TXX(AddressingMode::Implied, _y, _accumulator);  // TYA
+        case 0x48: return PHA(AddressingMode::Implied);                    // PHA
+        case 0x68: return PLA(AddressingMode::Implied);                    // PLA
+        case 0x08: return PHP(AddressingMode::Implied);                    // PHP
+        case 0x28: return PLP(AddressingMode::Implied);                    // PLP
+
+        // Other
+        case 0x00: return BRK(AddressingMode::Implied);
+        case 0xEA: return NOP(AddressingMode::Implied);
+
+        default: Log::Debug("Unknown opcode: $%02X", opcode); return 0;
+    };
 }
 
 void CPU::ExecuteNMI()
