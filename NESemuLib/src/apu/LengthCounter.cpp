5bdf0372a53f1f536fdcc9c8ccf3c030b9fd37fc
Kike Alcor <ealcor@gmail.com>
2018-09-12 09:13:22 +0200

    Move loadValues in the LengthCounter from the .h to the function body for consistency

--- NESemuLib/src/apu/LengthCounter.cpp
+++ NESemuLib/src/apu/LengthCounter.cpp
@@ -33,6 +33,10 @@ void LengthCounter::SetHalt(bool halt)
 
 void LengthCounter::SetLoad(int loadIndex)
 {
+    static const int loadValues[32] = {
+        10, 254, 20, 2, 40, 4, 80, 6, 160, 8, 60, 10, 14, 12, 26, 14, 12, 16, 24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30
+    };
+
     OMBAssert(loadIndex >= 0 && loadIndex <= 0x1F, "Invalid load value");
     if (_enabled)
     {
